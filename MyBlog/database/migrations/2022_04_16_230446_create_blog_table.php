68a5544ce64df4b1afe616c7a3390cdad5fecb90
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-04-17 00:03:51 +0900

    making migration 1

--- MyBlog/database/migrations/2022_04_16_230446_create_blog_table.php
+++ MyBlog/database/migrations/2022_04_16_230446_create_blog_table.php
@@ -13,9 +13,54 @@ class CreateBlogTable extends Migration
      */
     public function up()
     {
-        Schema::create('blog', function (Blueprint $table) {
-            $table->id();
+        Schema::create('category', function (Blueprint $table) {
+            $table->id('cat_id');
+            $table->string('cat_name', 20);
+
+            $table->primary('cat_id');
+        });
+
+        Schema::create('genre', function (Blueprint $table) {
+            $table->id('gen_id');
+            $table->string('gen_name', 20);
+
+            $table->primary('gen_id');
+        });
+
+        Schema::create('tag', function (Blueprint $table) {
+            $table->id('tag_id');
+            $table->string('tag_name', 40);
+
+            $table->primary('tag_id');
+        });
+
+        Schema::create('cat_gen', function (Blueprint $table) {
+            $table->bigInteger('cat_id');
+            $table->bigInteger('gen_id');
+
+            $table->primary(['cat_id', 'gen_id']);
+        });
+
+        Schema::create('post', function (Blueprint $table) {
+            $table->id('post_id');
+            $table->string('post_slag', 20);
+            $table->bigInteger('gen_id');
+            $table->string('post_title', 255);
+            $table->string('post_author', 20);
+            $table->text('post_content');
+            $table->string('post_stats', 10);
+            $table->integer('watch_count');
             $table->timestamps();
+            $table->timestamp('deleted_at');
+
+            $table->primary('post_id');
+        });
+
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->bigInteger('post_id');
+            $table->bigInteger('tag_id');
+
+            $table->primary(['post_id', 'tag_id']);
         });
     }
 

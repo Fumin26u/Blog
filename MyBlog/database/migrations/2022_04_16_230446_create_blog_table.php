1179423b47827e0940b0c3f96f536226c2fbdd23
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-04-17 03:08:53 +0900

    making migration 2

--- MyBlog/database/migrations/2022_04_16_230446_create_blog_table.php
+++ MyBlog/database/migrations/2022_04_16_230446_create_blog_table.php
@@ -17,50 +17,59 @@ class CreateBlogTable extends Migration
             $table->id('cat_id');
             $table->string('cat_name', 20);
 
-            $table->primary('cat_id');
+            // $table->primary('cat_id');
         });
 
         Schema::create('genre', function (Blueprint $table) {
             $table->id('gen_id');
             $table->string('gen_name', 20);
 
-            $table->primary('gen_id');
+            // $table->primary('gen_id');
         });
 
         Schema::create('tag', function (Blueprint $table) {
             $table->id('tag_id');
             $table->string('tag_name', 40);
 
-            $table->primary('tag_id');
+            // $table->primary('tag_id');
         });
 
         Schema::create('cat_gen', function (Blueprint $table) {
-            $table->bigInteger('cat_id');
-            $table->bigInteger('gen_id');
+            $table->unsignedBigInteger('cat_id');
+            $table->unsignedBigInteger('gen_id');
 
             $table->primary(['cat_id', 'gen_id']);
+
+            $table->foreign('cat_id')->references('cat_id')->on('category')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('gen_id')->references('gen_id')->on('genre')->onUpdate('cascade')->onDelete('cascade');
         });
 
         Schema::create('post', function (Blueprint $table) {
             $table->id('post_id');
             $table->string('post_slag', 20);
-            $table->bigInteger('gen_id');
+            $table->unsignedBigInteger('gen_id');
             $table->string('post_title', 255);
             $table->string('post_author', 20);
-            $table->text('post_content');
-            $table->string('post_stats', 10);
+            $table->text('post_content')->nullable();
+            $table->string('post_stats', 10)->default('pending');
             $table->integer('watch_count');
-            $table->timestamps();
-            $table->timestamp('deleted_at');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
 
-            $table->primary('post_id');
+            // $table->primary('post_id');
+
+            $table->foreign('gen_id')->references('gen_id')->on('genre')->onUpdate('cascade')->onDelete('cascade');
         });
 
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->bigInteger('post_id');
-            $table->bigInteger('tag_id');
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('tag_id');
 
             $table->primary(['post_id', 'tag_id']);
+
+            $table->foreign('post_id')->references('post_id')->on('post')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('tag_id')->references('tag_id')->on('tag')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
@@ -71,6 +80,13 @@ class CreateBlogTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blog');
+        Schema::dropIfExists('cat_gen');
+        Schema::dropIfExists('post_tag');
+        Schema::dropIfExists('post');
+        Schema::dropIfExists('tag');
+        Schema::dropIfExists('genre');
+        Schema::dropIfExists('category');
+        Schema::dropIfExists('migrations');
+        Schema::dropIfExists('personal_access_tokens');
     }
 }

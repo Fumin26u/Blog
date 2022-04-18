71f2821b84faf309867b3ba5b6c66ec899f69888
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-04-18 18:53:31 +0900

    ckeditor導入できた

--- MyBlog/resources/views/auth/index.blade.php
+++ MyBlog/resources/views/auth/index.blade.php
@@ -5,8 +5,18 @@
 <h1>test</h1>
 <p>only developers can access this site!</p>
 </main>
+<textarea id="ckeditor"></textarea>
 @endsection
 
 @section('script')
 <p>You can write Script at here!</p>
+<script src="https://cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+<script>
+    // エディタへの設定を適用する
+    CKEDITOR.replace('ckeditor', {
+      uiColor: '#EEEEEE',
+      height: 300,
+      width: 1200,
+    });
+  </script>
 @endsection
--- MyBlog/resources/views/templates/meta.blade.php
+++ MyBlog/resources/views/templates/meta.blade.php
@@ -1,3 +1,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="{{ asset("css/style.css") }}">
 <link rel="icon" href="{{ asset("images/favicon.png") }}">
+@yield('others_head')

80d03f6e3b4a1c6f41eb4eefe13ca1f23bc1a857
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-04-18 18:59:17 +0900

    ckeditor設定変更
    参考：https://qiita.com/anomeme/items/3bc2f1cd16fc4afa2d81

--- MyBlog/resources/views/auth/index.blade.php
+++ MyBlog/resources/views/auth/index.blade.php
@@ -14,9 +14,33 @@
 <script>
     // エディタへの設定を適用する
     CKEDITOR.replace('ckeditor', {
-      uiColor: '#EEEEEE',
-      height: 300,
-      width: 1200,
+        uiColor: '#EEEEEE',
+        height: 300,
+        width: 1200,
+        // エディタ内に適用するCSS
+        // contentsCss: [
+        //     "./css/sample.css",
+        // ],
+        // スペルチェック機能OFF
+        scayt_autoStartup: false,
+        // Enterを押した際に改行タグを挿入
+        enterMode: CKEDITOR.ENTER_BR,
+        // Shift+Enterを押した際に段落タグを挿入
+        shiftEnterMode: CKEDITOR.ENTER_P,
+        // idやclassを指定可能にする
+        allowedContent: true,
+        // ツールバーを下にする
+        toolbarLocation: 'bottom',
+        // preコード挿入時
+        format_pre: { element: 'pre', attributes: { 'class': 'code' } },
+        // タグのパンくずリストを削除
+        removePlugins: 'elementspath',
+        // webからコピペした際でもプレーンテキストを貼り付けるようにする
+        forcePasteAsPlainText: true,
+        // 自動で空白を挿入しないようにする
+        fillEmptyBlocks: false,
+        // タブの入力を無効にする
+        tabSpaces: 0,
     });
   </script>
 @endsection

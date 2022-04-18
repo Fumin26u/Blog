58e2ff513f7a441b1a52ddfe7f559368b3c52938
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-04-18 18:19:14 +0900

    @extends, @include, @section, @yieldが使えるようになった

--- MyBlog/resources/views/auth/index.blade.php
+++ MyBlog/resources/views/auth/index.blade.php
@@ -1,1 +1,12 @@
-only developers can access this site!
+@extends('template')
+
+@section('content')
+<main>
+<h1>test</h1>
+<p>only developers can access this site!</p>
+</main>
+@endsection
+
+@section('script')
+<p>You can write Script at here!</p>
+@endsection
---
+++ MyBlog/resources/views/templates/meta.blade.php
@@ -0,0 +1,3 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{{ asset("css/style.css") }}">
+<link rel="icon" href="{{ asset("images/favicon.png") }}">

37c4212f68052d83b8eec05c49f07a45b01ed481
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-05-17 10:41:27 +0900

    fix

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -12,28 +12,28 @@
         <div>
             <dt>スラッグ</dt>
             <dd>
-                <input type="text" name="slag" value="{{ $post->post_slag }}" maxlength="20">
+                <input type="text" name="slag" value="@if(isset($post)) {{ $post->post_slag }} @endif" maxlength="20">
             </dd>
         </div>
         <div>
             <dt>タイトル</dt>
             <dd>
-                <input type="text" name="title" value="{{ $post->post_title }}" maxlength="255">
+                <input type="text" name="title" value="@if(isset($post)) {{ $post->post_title }} @endif" maxlength="255">
             </dd>
         </div>
         <div>
             <dt>内容</dt>
             <dd>
-                <textarea name="content" id="ckeditor" value="">{{ $post->post_content }}</textarea>
+                <textarea name="content" id="ckeditor" value="">@if(isset($post)) {{ $post->post_content }} @endif</textarea>
             </dd>
         </div>
         <div>
             <dt>公開設定</dt>
             <dd>
                 <select name="stats" id="">
-                    <option value="pending" @if($post->post_stats === 'pending') selected @endif>編集中</option>
-                    <option value="private" @if($post->post_stats === 'private') selected @endif>非公開</option>
-                    <option value="public" @if($post->post_stats === 'public') selected @endif>公開</option>
+                    <option value="pending" @if(isset($post) && $post->post_stats === 'pending') selected @endif>編集中</option>
+                    <option value="private" @if(isset($post) && $post->post_stats === 'private') selected @endif>非公開</option>
+                    <option value="public" @if(isset($post) && $post->post_stats === 'public') selected @endif>公開</option>
                 </select>
             </dd>
         </div>

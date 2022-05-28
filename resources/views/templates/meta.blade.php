f01b5aa0db6fc28c35dcbcdcf5323220149e95bb
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-05-28 09:18:52 +0900

    gtag追加

--- resources/views/templates/meta.blade.php
+++ resources/views/templates/meta.blade.php
@@ -4,6 +4,15 @@
 <link rel="icon" href="{{ asset("images/favicon.png") }}">
 {{-- Load codesnippets style of CKEditor --}}
 <link href="{{ asset('ckeditor/plugins/codesnippet/lib/highlight/styles/default.css') }}" rel="stylesheet">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-142586151-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-142586151-1');
+</script>
 <script src="{{ asset("ckeditor/plugins/codesnippet/lib/highlight/highlight.pack.js") }}"></script>
 <script>hljs.initHighlightingOnLoad();</script>
 @yield('others_head')
